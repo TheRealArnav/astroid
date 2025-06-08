8fd3313b0b6a988da0203f1a11714a23e168b5b2
TheRealArnav <tyagi.arnav02@gmail.com>
2025-06-08 14:55:53 +0200

    Add files via upload

--- astroid.py
+++ astroid.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import time
+import math
 pygame.init()
 pygame.font.init()
 HEIGHT = 800
@@ -11,7 +12,7 @@ pygame.display.set_caption("ASTROID DESTROYER")
 
 fps = 60
 clock = pygame.time.Clock()
-
+count = 0
 
 small_astroid = pygame.image.load("C:/Pygame2/images/astroid.png")
 medium_astroid = pygame.image.load("C:/Pygame2/images/mediumastroid.png")
@@ -21,6 +22,25 @@ alien_ship = pygame.image.load("C:/Pygame2/images/alienship.png")
 
 rocketship = pygame.image.load("C:/Pygame2/images/spaceship.png")
 background = pygame.image.load("C:/Pygame2/images/spacebg3.png")
+
+
+class Bullet():
+    def __init__(self,x,y,angle):
+        self.x = x
+        self.y = y
+        angle = math.radians(angle)
+        self.speed = 10
+        self.dx = math.sin(angle)
+        self.dy = math.cos(angle)
+    def move(self):
+        self.y = self.y - (self.dy * self.speed)
+        self.x = self.x - (self.dx * self.speed)
+    def draw(self):
+        pygame.draw.rect(screen,"yellow",(self.x,self.y,5,20))
+
+        
+
+
 
 class Rocket(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -56,9 +76,9 @@ class Astroid(pygame.sprite.Sprite):
         super().__init__()
         self.dir = dir
         self.size = size
+        self.x = random.randint(0,800)
+        self.y = random.randint(0,800)
         self.image = pygame.image.load("C:/Pygame2/images/astroid.png")
-    def draw(self):
-
         if self.size == 1:
             self.image = small_astroid
         if self.size == 2:
@@ -71,16 +91,35 @@ class Astroid(pygame.sprite.Sprite):
             self.y = -15
             self.x = random.randint(0,WIDTH)
         if self.dir == "bottom":
-            self.y = 815
+            self.y = 715
             self.x = random.randint(0,WIDTH)
         if self.dir == "left":
             self.x = -15
             self.y = random.randint(0,HEIGHT)
         if self.dir == "right":
-            self.x = 815 
+            self.x = 715 
             self.y = random.randint(0,HEIGHT)
+    def draw(self):      
+
         screen.blit(self.image,(self.x,self.y))
-        
+    def move(self):
+        directionlist = [-1,1]
+        if self.dir == "top":
+            x = random.choice(directionlist)
+            self.x = self.x - x
+            self.y = self.y + 1
+        if self.dir == "bottom":
+            x = random.choice(directionlist)
+            self.x = self.x - x
+            self.y = self.y - 1
+        if self.dir == "left":
+            x = random.choice(directionlist)
+            self.y = self.y - x
+            self.x = self.x + 1
+        if self.dir == "right":
+            x = random.choice(directionlist)
+            self.y = self.y - x
+            self.x = self.x - 1 
 
 
 
@@ -88,23 +127,29 @@ class Astroid(pygame.sprite.Sprite):
 rocket = Rocket(400,400)
 
 
-astroidgroup = pygame.sprite.Group()
+astroidlist = []
 
-
+for i in range(10): 
+    size = random.randint(1,3)
+    direclist = ["top","bottom","right","left"]
+    y = random.choice(direclist)
+    direction = y
+    astroid = Astroid(direction,size)
+        
+    astroidlist.append(astroid)
+        
+bulletlist = []
 
 run = True
 while run:
     clock.tick(fps)
-    for i in range(30):
-        size = random.randint(1,3)
-        direclist = ["top","bottom","right","left"]
-        y = random.randint(0,3)
-        direction = direclist[y]
-        astroid = Astroid(direction,size)
+    count = count + 1
+    for astroid in astroidlist:
+        astroid.move()
         astroid.draw()
-        astroidgroup.add(astroid)
         pygame.display.update()
-
+        
+        
 
     screen.blit(background,(0,0))
     rocket.draw()
@@ -117,14 +162,21 @@ while run:
         rocket.forward()
     if keys[pygame.K_DOWN]:
         rocket.backward()
-
-    pygame.display.update()
+    
+    #pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bullet = Bullet(rocket.x, rocket.y - 0, rocket.angle)
+                bulletlist.append(bullet)
+                
         #if event.type == pygame.key.get_pressed():
         
             
             
-
-    pygame.display.flip()
+    for bullet in bulletlist:
+        bullet.draw()
+        bullet.move()
+    pygame.display.update()
